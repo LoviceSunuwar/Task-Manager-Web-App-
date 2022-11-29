05f28b83f56c2a846aede0e51c902f5f49073ef5
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-11-29 10:40:50 -0500

    final commit for the course project

--- backend/middlewares/isLoggedIn.js
+++ backend/middlewares/isLoggedIn.js
@@ -1,11 +1,42 @@
+// const jwt = require("jsonwebtoken");
+// const isLoggedIn = (req, res, next) => {
+//   const token = req.get("authorization");
+//   const verified = jwt.verify(token, "loginsecret");
+//   if (!verified) {
+//     res.redirect("/user/login");
+//   }
+//   console.log(verified);
+//   next();
+// };
+
+// module.exports = isLoggedIn;
+
+// require('dotenv')
+// import * as jwt from 'jsonwebtoken'
 const jwt = require("jsonwebtoken");
+
 const isLoggedIn = (req, res, next) => {
+  
   const token = req.get("authorization");
-  const verified = jwt.verify(token, "loginsecret");
+  const secret = process.env.SECRET;
+  // const token = process.env.SECRET;
+
+  
+  // console.log('jwt',jwt);
+  // console.log('secret',process.env.SECRET);
+  const verified = jwt.verify(token, secret);
+  // const verified = jwt.verify(token, "THISISTHESECRET");
+  //console.log(jwt.verify(token, "THISISTHESECRET"));
   if (!verified) {
+    console.log('not verified');
+
     res.redirect("/user/login");
   }
-  next();
+  if (verified){
+    //console.log('verified');
+    next();
+  }
+  
 };
 
 module.exports = isLoggedIn;
